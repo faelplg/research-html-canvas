7e581b58ddd748b8d8e84d14b581d8ea929e223b
Fael <faelplg@gmail.com>
2025-06-15 01:58:46 -0300

    finishing glyphs study

--- sketches/sketch-05-b.js
+++ sketches/sketch-05-b.js
@@ -25,12 +25,12 @@ const sketch = ({ context, width, height }) => {
   typeCanvas.height = rows;
 
   return ({ context, width, height }) => {
-    typeContext.fillStyle = 'white';
+    typeContext.fillStyle = 'black';
     typeContext.fillRect(0, 0, cols, rows);
 
-    fontSize = cols;
+    fontSize = cols * 1.2;
 
-    typeContext.fillStyle = 'black';
+    typeContext.fillStyle = 'white';
     typeContext.font = `${fontSize}px ${fontfamily}`;
     typeContext.textBaseline = 'top';
 
@@ -56,7 +56,12 @@ const sketch = ({ context, width, height }) => {
     const typeData = typeContext.getImageData(0, 0, cols, rows).data;
     console.log(`${typeData.length} pixels`);
 
-    context.drawImage(typeCanvas, 0, 0);
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, width, height);
+
+    context.textBaseline = 'middle';
+    context.textAlign = 'center';
+    // context.drawImage(typeCanvas, 0, 0);
 
     for (let i = 0; i < numCells; i++) {
       const col = i % cols;
@@ -70,18 +75,36 @@ const sketch = ({ context, width, height }) => {
       const b = typeData[i * 4 + 2];
       const a = typeData[i * 4 + 3];
 
-      context.fillStyle = `rgba(${r}, ${g}, ${b}, ${a / 255})`;
+      const glyph = getGlyph(r);
+      context.font = `${cell * 2}px ${fontfamily}`;
+      if (Math.random() < 0.1) {
+        context.font = `${cell * 6}px ${fontfamily}`;
+      }
+
+      // context.fillStyle = `rgba(${r}, ${g}, ${b}, ${a / 255})`;
+      context.fillStyle = 'white';
 
       context.save();
       context.translate(x, y);
       context.translate(cell * 0.5, cell * 0.5);
 
-      context.beginPath();
-      context.arc(0, 0, cell * 0.5, 0, Math.PI * 2);
-      context.fill();
+      // context.beginPath();
+      // context.arc(0, 0, cell * 0.5, 0, Math.PI * 2);
+      // context.fill();
+
+      context.fillText(glyph, 0, 0);
       context.restore();
     }
   };
+};
+
+const getGlyph = v => {
+  if (v < 50) return '';
+  const glyphs = 'fail';
+  const index = Math.floor((v / 255) * (glyphs.length - 1));
+  // if (Math.random() < 0.1)
+  return glyphs[Math.floor(Math.random() * glyphs.length)];
+  // return glyphs[index];
 };
 
 document.addEventListener('keyup', e => {
